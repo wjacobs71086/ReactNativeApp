9ad6f7be54a3be824b50b6df9a4d42ccf124573f
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-02-01 20:27:31 -0800

    successful api calls

--- App/App.js
+++ App/App.js
@@ -1,17 +1,80 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-
+import {StyleSheet, View, Text, Image} from 'react-native';
+import bernie from './assets/FullSizeRender.jpeg';
 import FetchLocation from './components/FetchLocation';
 
+const styles = StyleSheet.create({
+  bigBlue: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 75,
+    alignSelf: 'center',
+  },
+  red: {
+    display: 'flex',
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+  },
+});
+
 export default class App extends React.Component {
-  getUserLocationHandler = () => {
-    console.log('you son of a bitch, Im in');
+  state = {
+    dog: false,
+    imgUrl: '',
   };
+
+  componentDidMount() {
+    return fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          imgUrl: responseJson.message,
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  getNewDog() {
+    return fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          imgUrl: responseJson.message,
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  getUserLocationHandler = () => {
+    this.setState({
+      dog: true,
+    });
+    this.getNewDog();
+  };
+  //require('./assets/FullSizeRender.jpeg')
+  renderDogImg = () => {
+    if (this.state.dog) {
+      console.log(this.state.dog);
+      return (
+        <Image
+          source={{uri: `${this.state.imgUrl}`}}
+          style={{width: 400, height: 400}}
+        />
+      );
+    }
+  };
+
   render() {
     return (
-      <View>
-        <Text>My Text</Text>
+      <View style={styles.red}>
+        <Text style={styles.bigBlue}>My Text</Text>
         <FetchLocation onGetLocation={this.getUserLocationHandler} />
+        {this.renderDogImg()}
       </View>
     );
   }
--- App/components/FetchLocation.js
+++ App/components/FetchLocation.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import {Button} from 'react-native';
+import {Button, StyleSheet, View} from 'react-native';
+
+const buttonStyles = StyleSheet.create({
+  button: {
+    backgroundColor: 'white',
+    borderRadius: 25,
+    padding: 10,
+    fontSize: 10,
+    margin: 20,
+  },
+});
 
 const fetchLocation = props => {
-  return <Button title="Get Location" onPress={props.onGetLocation} />;
+  return (
+    <View style={buttonStyles.button}>
+      <Button
+        style={buttonStyles.button}
+        color={'grey'}
+        fontSize={'80'}
+        title="Get Locations"
+        onPress={props.onGetLocation}
+      />
+    </View>
+  );
 };
 
 export default fetchLocation;
